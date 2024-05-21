47b39aa9e1c7c7d69bf942d135c552871fc209e1
maoxian636 <2572392156@qq.com>
2024-05-21 15:45:27 +0800

    2

--- app/src/main/java/com/yangzai/superapp/ui/MainActivity.kt
+++ app/src/main/java/com/yangzai/superapp/ui/MainActivity.kt
@@ -2,14 +2,23 @@ package com.yangzai.superapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.yangzai.superapp.R
+import com.msdk.xsdk.MSDKSingleton
 import com.msdk.xsdk.Test
+import com.msdk.xsdk.bean.XConfigData
+import com.msdk.xsdk.utils.XException
+import com.yangzai.superapp.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main)
-        Test().A
+        try {
+            MSDKSingleton.getInstance(this).setMSDKConfig(XConfigData("xxxx", "aaaaaa"))
+            MSDKSingleton.setDebug(true)
+            MSDKSingleton.getInstance(this).initConfig()
+        } catch (e: XException) {
+            throw RuntimeException(e)
+        }
 
     }
 }
